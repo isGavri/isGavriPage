4933478e4cc305e8581622909818c0e90b6864b8
Estefan Gabriel Partida Toalá <zssykes1@gmail.com>
2023-11-12 01:01:03 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27092,7 +27092,7 @@ var _stylesCss = require("./styles.css");
     email: "zssykes1@gmail.com",
     gitHub: "https://github.com/isGavri",
     instagram: "",
-    linkedIn: "https://www.linkedin.com/in/estefan-gabriel-partida-toal%C3%A1-6004ba294",
+    linkedIn: "estefan-gabriel-partida-toal%C3%A1-6004ba294",
     medium: "",
     twitter: "",
     youTube: ""
